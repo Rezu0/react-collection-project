ee9866a06de6057c85a4b2eaea7c8b3e4c718b7b
Rezu0 <yunianrezky031@gmail.com>
2024-07-10 02:40:45 +0700

    handler fetching untuk prove project

--- src/utils/handler-fetching.js
+++ src/utils/handler-fetching.js
@@ -20,3 +20,25 @@ export async function handlerFetchingAllSaldoStaff (token) {
     throw err;
   }
 }
+
+export async function handlerFetchingProveProject (token, data) {
+  const headerProveProject = new Headers();
+  headerProveProject.append("Content-Type", "application/json");
+  headerProveProject.append("Authorization", `Bearer ${token}`);
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: headerProveProject,
+    body: JSON.stringify(data),
+    redirect: 'follow'
+  };
+
+  try {
+    const responseFetchingProve = await fetch(`${LINK_API}api/prove-project`, requestOptions);
+    const returnData = await responseFetchingProve.json();
+    return returnData;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
